30e92ba0147e994d68888adc790007a3d0575c82
CaniTanushaj <hasan.tanushaj@fsre.sum.ba>
2023-05-22 19:23:21 +0200

    2.x zadace

---
+++ DZ1.3/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\ActorsController;
+use App\Http\Controllers\CastController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\LiveSearchController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::resource('movies', MovieController::class);
+Route::resource('actors',ActorsController::class);
+Route::resource('cast',CastController::class);
+
+Route::get('/',function(){
+    return redirect('/movies');
+});
+
+Route::get('/movies.orderbyDate',[MovieController::class, 'orderbyDate']);
+Route::get('/actors.orderbyBirthDate',[ActorsController::class, 'orderbyBirthDate']);
+Route::get('/movies.fetch', [MovieController::class, 'fetch'])->name('movie.fetch');
+Route::get('/actors.fetch', [ActorsController::class, 'fetch'])->name('actors.fetch');
+
+
